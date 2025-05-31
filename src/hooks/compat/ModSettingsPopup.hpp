fa503b2ba93df6c04ec148df640771c528a78a4f
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2025-05-31 19:35:08 +0100

    ok we're bringing it back

---
+++ src/hooks/compat/ModSettingsPopup.hpp
@@ -0,0 +1,6 @@
+#pragma onceAdd commentMore actions
+#include <alphalaneous.alphas_geode_utils/include/NodeModding.h>
+
+class $nodeModify(HookedModSettingsPopup, ModSettingsPopup) {
+    void modify();
+};
