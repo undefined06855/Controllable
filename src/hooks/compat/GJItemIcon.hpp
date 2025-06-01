f2207dde0c988863a77fdbaa88244c4ddee373cb
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2025-06-01 16:09:06 +0100

    testing gjitemicon "fixes"

--- src/hooks/compat/GJItemIcon.hpp
+++ src/hooks/compat/GJItemIcon.hpp
@@ -2,5 +2,5 @@
 #include <Geode/modify/GJItemIcon.hpp>
 
 class $modify(HookedGJItemIcon, GJItemIcon) {
-    bool init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7);
+    bool init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6/*, cocos2d::ccColor3B p7*/);
 };
