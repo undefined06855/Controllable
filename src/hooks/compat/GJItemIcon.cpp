2a2e575bed7da4ac517b7ff7440676f6eda39b45
undefined06855 <currentcashew57@gmail.com>
2025-06-02 11:39:24 +0100

    ok gjitemicon was just debug issues

--- src/hooks/compat/GJItemIcon.cpp
+++ src/hooks/compat/GJItemIcon.cpp
@@ -1,7 +1,7 @@
 #include "GJItemIcon.hpp"
 
-bool HookedGJItemIcon::init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6/*, cocos2d::ccColor3B p7*/) {
-    if (!GJItemIcon::init(p0, p1, p2, p3, p4, p5, p6/*, p7*/)) return false;
+bool HookedGJItemIcon::init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7) {
+    if (!GJItemIcon::init(p0, p1, p2, p3, p4, p5, p6, {})) return false;
 
     setUserObject("force-shadowed-selection"_spr, cocos2d::CCBool::create(true));
 
