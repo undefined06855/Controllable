d7aa190d29c26c2c3866a55457cc293c3e015ea3
undefined06855 <currentcashew57@gmail.com>
2025-06-02 12:41:12 +0100

    gjitemicon only in release

--- src/hooks/compat/GJItemIcon.cpp
+++ src/hooks/compat/GJItemIcon.cpp
@@ -1,5 +1,7 @@
 #include "GJItemIcon.hpp"
 
+#ifndef REMOVE_GJITEMICON
+
 bool HookedGJItemIcon::init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7) {
     if (!GJItemIcon::init(p0, p1, p2, p3, p4, p5, p6, {})) return false;
 
@@ -7,3 +9,5 @@ bool HookedGJItemIcon::init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2
 
     return true;
 }
+
+#endif
--- src/hooks/compat/GJItemIcon.hpp
+++ src/hooks/compat/GJItemIcon.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <Geode/modify/GJItemIcon.hpp>
 
+#ifndef REMOVE_GJITEMICON
+
 class $modify(HookedGJItemIcon, GJItemIcon) {
     bool init(UnlockType p0, int p1, cocos2d::ccColor3B p2, cocos2d::ccColor3B p3, bool p4, bool p5, bool p6, cocos2d::ccColor3B p7);
+
 };
+
+#endif
