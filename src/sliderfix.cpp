a5e3d2ea59737c8dfa6b3693783475f0cca349b0
undefined06855 <currentcashew57@gmail.com>
2025-06-05 16:54:05 +0100

    use geode util

--- src/sliderfix.cpp
+++ src/sliderfix.cpp
@@ -2,6 +2,6 @@ $on_mod(Loaded) {
     auto path = geode::Mod::get()->getResourcesDir();
     cocos2d::CCFileUtils::get()->addTexturePack({
         .m_id = "sliderfix"_spr,
-        .m_paths = { path.string() }
+        .m_paths = { geode::utils::string::pathToString(path) }
     });
 }
